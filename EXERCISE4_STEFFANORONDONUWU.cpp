b583bfd37da33464e877cdc4392c2d10c448b42c
steffanorondonuwu <79073508+steffanorondonuwu@users.noreply.github.com>
2021-04-19 12:28:11 +0800

    Add files via upload

---
+++ EXERCISE4_STEFFANORONDONUWU.cpp
@@ -0,0 +1,61 @@
+// POLYMORPHISM //
+// STEFFANO IMMANUEL RONDONUWU //
+// EXERCISE 4 //
+
+#include <iostream>
+#include <string>
+#include <iomanip>
+
+using namespace std;
+
+struct employee {
+	string name, department;
+	int age;
+	char gender;
+	float days, rate;
+};
+
+main(){
+	
+	float solve=0.00;
+	string sex;
+	employee user;
+	
+	cout << "\t\tEmployee's Payroll System in C++";
+	cout << "\n\n";
+	cout << "\t Created BY: Steffano Immanuel Rondonuwu";
+	cout << "\n\n";
+	cout << "Enter the Name of Employee       : ";
+	getline(cin,user.name);
+	cout << "Enter the Name of Department     : ";
+	getline(cin,user.name);
+	cout << "Enter the Gender of the Employee : ";
+	user.gender = getchar();
+	cout << "Enter the Number of Days Worked  : ";
+	cin >>user.days;
+	cout << "Enter the Daily Rate             : ";
+	cin >> user.rate;
+	
+	solve = (user.days * user.rate);
+	if (user.gender == 'M' || user.gender == 'm')
+	{
+		sex = "Male";
+	}
+	else if (user.gender == 'F' || user.gender == 'f')
+	{
+		sex = "Female";
+	}
+	cout << fixed;
+	cout << setprecision(2);
+	    cout << "\n\n";
+	    cout << "\t ===== GENERATED REPORT =====";
+	    cout << "\n\n";
+	    cout << "\nEmployee Name : " << user.name;
+		cout << "\nDepartment    : " << user.department;
+		cout << "\nGender        : " << sex;
+		cout << "\nSalary        : $" << solve;
+		cout << "\n\n";
+		system("pause"); 
+	
+}
+
